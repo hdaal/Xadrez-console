f4350ed939c4239e726140a0fe2c7066c9b9785a
hdaal <henrique.daal@outlook.com>
2020-03-13 15:40:30 -0300

    Projeto criado

---
+++ xadrex/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace xadrex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

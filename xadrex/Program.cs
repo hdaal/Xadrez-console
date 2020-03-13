35d8982c65e49cdfbc9cec8f409c3ec9e96c02e4
hdaal <henrique.daal@outlook.com>
2020-03-13 16:03:58 -0300

    Criando a classe Posicao

--- xadrex/Program.cs
+++ xadrex/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Tabuleiro;
 
 namespace xadrex
 {
@@ -6,7 +7,11 @@ namespace xadrex
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Posicao P;
+
+            P = new Posicao(3, 4);
+
+            Console.WriteLine(P);
         }
     }
 }
